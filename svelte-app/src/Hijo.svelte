e6396ed84177f979f8dae2605c8fb086cbd9dfd2
yo <yo@fei.uv>
2025-11-27 15:27:05 -0600

    svelte-SaludarDatos

---
+++ svelte-app/src/Hijo.svelte
@@ -0,0 +1,18 @@
+<script>
+    // código js
+    export let texto = 'Boton';
+    export let color = 'green';
+</script>
+
+<!-- Markup html -->
+<button style="--btn-color: {color}">
+    {texto}
+</button>
+
+<style>
+    /* código css */
+    button {
+        color: white;
+        background-color: var(--btn-color);
+    }
+</style>
